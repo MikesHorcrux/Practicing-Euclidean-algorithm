33c7ea38459f25975f0f30bd9ba6a82cb3bb7f0a
Mike Van Amburg <vanamburg.mv@gmail.com>
2019-10-29 16:09:15 -0600

    Euclidean Algorithm

---
+++ GCP.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Practicing_algorithms
+{
+    public class GCD
+    {
+
+        public int gcd(int a, int b)
+        {
+            //find the greatest common Denominatior of two numbers 
+            //using Euclids Algorithum 
+            if (a == 0)
+            {
+                return b;
+            }
+            return gcd(b % a, a);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practicing_algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Euclidean
+            Console.WriteLine("Euclidean Example");
+            GCD euclidean = new GCD();
+            Console.WriteLine("Enter a Number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a Number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("greatest common divisor is: " + euclidean.gcd(a, b));        }
+    }
+}
